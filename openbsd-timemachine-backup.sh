a7202f7b8a64cb3150465d20c7089a6bd6e77bb6
the xhr <xhr@giessen.ccc.de>
2022-06-14 12:42:28 +0200

    Fix spacing issues
    Somehow the import got messed up and spaces were missing.

--- openbsd-timemachine-backup.sh
+++ openbsd-timemachine-backup.sh
@@ -14,71 +14,73 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+# Mountpoint for the backup disk
 MNTPOIN=/backup
-#DUID of the softraid container
-OUTER_DUID=$1
-#DUID of the disk within the decrypted container
-INNER_DUID=$2
-#Location to the file containing the passphrase
-PASSFILE=$3
-#Location to counter file
-CNTF=${MNTPOIN}/.counter
 
-#Wrong number of arguments
-if[[ -z $OUTER_DUID || -z $INNER_DUID ]]; then
-	logger-i -t error "$(basename $0): DUIDs missing. Abort"
-	exit1
+# ------- Do not modify the following variables ----------
+# DUID of the softraid container
+OUTER_DUID=$1
+# DUID of the disk within the decrypted container
+INNER_DUID=$2
+# Location to the file containing the passphrase
+PASSFILE=$3
+# Location to counter file
+CNTF=${MNTPOIN}/.counter
+# --------------------------------------------------------
+
+# Wrong number of arguments
+if [[ -z $OUTER_DUID || -z $INNER_DUID ]]; then
+	logger -i -t error "$(basename $0): DUIDs missing. Abort"
+	exit 1
 fi
 
-if[[ -z $PASSFILE ]]; then
-	logger-i -t error "$(basename $0): No path to PASSFILE given. Abort"
-	exit1
+if [[ -z $PASSFILE ]]; then
+	logger -i -t error "$(basename $0): No path to PASSFILE given. Abort"
+	exit 1
 else
-	if[[ ! -f $PASSFILE ]]; then
-		logger-i -t error "$(basename $0): Cannot open $PASSFILE. Abort"
-		exit1
+	if [[ ! -f $PASSFILE ]]; then
+		logger -i -t error "$(basename $0): Cannot open $PASSFILE. Abort"
+		exit 1
 	fi
 fi
 
-if[[ -n $(mount | grep {$MNTPOIN}) ]]; then
-	logger-i -t error "$(basename $0): Mount point $MNTPOIN is not empty. Abort"
-	exit1
+if [[ -n $(mount | grep ${MNTPOIN}) ]]; then
+	logger -i -t error "$(basename $0): Mount point $MNTPOIN is not empty. Abort"
+	exit 1
 fi
 
-bioctl-c C -p $PASSFILE -l ${OUTER_DUID}.a softraid0 > /dev/null || exit 1
-logger"$(basename $0): Backup disk successfully bio-attached"
+bioctl -c C -p $PASSFILE -l ${OUTER_DUID}.a softraid0 > /dev/null || exit 1
+logger "$(basename $0): Backup disk successfully bio-attached"
 
 sync
 
-mount-o softdep,noatime ${INNER_DUID}.i $MNTPOIN || exit 1
-logger"$(basename $0): Backup disk mounted successfully to $MNTPOIN"
+mount -o softdep,noatime ${INNER_DUID}.i $MNTPOIN || exit 1
+logger "$(basename $0): Backup disk mounted successfully to $MNTPOIN"
 
-#exit0
-
-#First backup of its kind
-if[[ ! -f ${CNTF} ]]; then
-	echo"1" > $CNTF
+# First backup of its kind
+if [[ ! -f ${CNTF} ]]; then
+	echo "1" > $CNTF
 fi
 
-i=$(cat$CNTF)
-if[ $((i%8)) -eq 0 ]; then
-	logger"$(basename $0): Iteration ${i}, doing a gamma backup"
-	rsnapshot-q gamma
-elif[ $((i%4)) -eq 0 ]; then
-	logger"$(basename $0): Iteration ${i}, doing a beta backup"
-	rsnapshot-q beta
+i=$(cat $CNTF)
+if [ $((i%8)) -eq 0 ]; then
+	logger "$(basename $0): Iteration ${i}, doing a gamma backup"
+	rsnapshot -q gamma
+elif [ $((i%4)) -eq 0 ]; then
+	logger "$(basename $0): Iteration ${i}, doing a beta backup"
+	rsnapshot -q beta
 else
-	logger"$(basename $0): Iteration ${i}, doing an alpha backup"
-	rsnapshot-q alpha
+	logger "$(basename $0): Iteration ${i}, doing an alpha backup"
+	rsnapshot -q alpha
 fi
-echo$((i+=1)) > $CNTF
+echo $((i+=1)) > $CNTF
 
 sync
 
-umount$MNTPOIN || exit 1
+umount $MNTPOIN || exit 1
 
-logger"$(basename $0): $MNTPOIN successfully unmounted"
+logger "$(basename $0): $MNTPOIN successfully unmounted"
 
-bioctl-d $INNER_DUID || exit 1
+bioctl -d $INNER_DUID || exit 1
 
-logger"$(basename $0): disk successfully bio-detached"
+logger "$(basename $0): disk successfully bio-detached"
